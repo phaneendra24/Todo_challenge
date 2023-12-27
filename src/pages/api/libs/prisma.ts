ad93b009e60584910f4dd73053b3d107865cf754
phaneendra <phaneendrapilli777@gmail.com>
2023-12-27 17:05:59 +0530

    prisma edge instance

--- src/pages/api/libs/prisma.ts
+++ src/pages/api/libs/prisma.ts
@@ -12,14 +12,14 @@
 // }
 
 // export default prisma;
-import { PrismaClient } from "@prisma/client";
+
+import { PrismaClient } from "@prisma/client/edge";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
